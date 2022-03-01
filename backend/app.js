e5424e02958396e10d897b87d59b14e928047a50
seungguini <sl6369@nyu.edu>
2022-02-28 22:48:15 -0500

    Fix res.json accessToken typo bug

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,6 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.post("/login", (req, res) => {
-  console.log(req.body)
-
   const spotifyAPI = new SpotifyWebAPI({
     redirectUri: process.env.REDIRECT_URI,
     clientId: process.env.CLIENT_ID,
@@ -22,11 +20,47 @@ app.post("/login", (req, res) => {
 
   // client-side Auth code for each user
   const code = req.body.code
-
+  console.log("HI")
+  console.log(code)
   // Asynchronously authenticate Spotify user
   spotifyAPI
     .authorizationCodeGrant(code)
     .then((data) => {
+      // console.log(data)
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      })
+    })
+    .catch((err) => {
+      // console.log("ERROR LOGIN")
+      // console.log(code)
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
+
+// Route to refresh authorization token
+app.post("/refresh", (req, res) => {
+  console.log("REFRESH BACKEND")
+  console.log(req.body)
+  const refreshToken = req.body.refreshToken
+  const spotifyAPI = new SpotifyWebAPI({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: refreshToken,
+  }) // Instantiate new API
+
+  spotifyAPI
+    .refreshAccessToken()
+    .then((data) => {
+      // console.log("The access token has been refreshed!")
+
+      // Save access token for future calls
+      spotifyAPI.setAccessToken(data.body["access_token"])
+
       res.json({
         accesToken: data.body.access_token,
         refreshToken: data.body.refresh_token,
@@ -34,24 +68,10 @@ app.post("/login", (req, res) => {
       })
     })
     .catch((err) => {
+      console.log("Failed to refresh access token")
       console.log(err)
-      res.sendStatus(400)
     })
 })
-
-app.get("/login", (req, res) => {
-  console.log("login get route")
-  res.json("hi")
-})
-
-app.get("/", (req, res) => {
-  console.log("main route")
-  res.json("main route")
-})
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
 
 // export the express app we created to make it available to other modules
 module.exports = app
