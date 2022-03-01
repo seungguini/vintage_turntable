c733277695bf5cf895e6d5c025f4602926831f0d
seungguini <sl6369@nyu.edu>
2022-02-28 22:48:00 -0500

    Create reactive spotify search bar

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,8 +1,73 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import useAuth from "./useAuth"
+import { Container, Form } from "react-bootstrap"
+import SpotifyWebApi from "spotify-web-api-node"
+import axios from "axios"
+
+const server_url = process.env.REACT_APP_SERVER_URL
+
+const spotifyAPI = new SpotifyWebApi({
+  clientId: process.env.REACT_APP_CLIENT_ID,
+})
 const Dashboard = ({ code }) => {
-  useAuth(code)
-  return <div>{code}</div>
+  let accessToken = ""
+  let refreshToken = ""
+  let expiresIn = 0
+
+  // Update auth states whenever there is a new auth code from the client side
+  useEffect(() => {
+    console.log("Getting access token and saving to localStorage!")
+    console.log(code)
+
+    // Access token not found in local storage - making Post req. to server to get new tokens
+    axios
+      .post(server_url + "/login", { code })
+      .then((res) => {
+        console.log("setting values")
+        console.log(res)
+        localStorage.setItem("accessToken", res.data.accessToken)
+        localStorage.setItem("refreshToken", res.data.refreshToken)
+        localStorage.setItem("expiresIn", res.data.expiresIn)
+
+        spotifyAPI.setAccessToken(res.data.accessToken)
+
+        // window.history.pushState({}, null, "/")
+      })
+      .catch((err) => {
+        console.log(err)
+        window.location = "/" // Redirect to login screen
+      })
+  }, [code])
+
+  const [search, setSearch] = useState("")
+  const [searchResults, setSearchResults] = useState([])
+
+  useEffect(() => {
+    if (!search) {
+      console.log("No search!")
+      setSearchResults([])
+      return
+    }
+
+    spotifyAPI.searchTracks(search).then((res) => {
+      setSearchResults(res.body.tracks.items)
+    })
+  }, [search, accessToken])
+
+  return (
+    <Container className="d-flex flex-column py-2">
+      {code}
+      <Form.Control
+        type="search"
+        placeholder="Search Songs / Artists"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {searchResults.map((result, idx) => {
+        return <p key={idx}>{result.name}</p>
+      })}
+    </Container>
+  )
 }
 
 export default Dashboard
--- frontend/src/useAuth.js
+++ frontend/src/useAuth.js
@@ -9,11 +9,10 @@ import axios from "axios"
 const server_url = process.env.REACT_APP_SERVER_URL
 
 export default function useAuth(code) {
-  const [accessToken, setAccessToken] = useState()
-  const [refreshToken, setRefreshToken] = useState()
-  const [expiresIn, setExpiresIn] = useState()
-
   console.log("runnig useAuth")
+
+  // Recursive function to refresh tokens 1 minute before it expires
+  const refreshToken = () => {}
 
   // Update auth states whenever there is a new auth code from the client side (i.e. new update)
   useEffect(() => {
@@ -24,35 +23,38 @@ export default function useAuth(code) {
       .then((res) => {
         console.log("setting values")
         console.log(res)
-        setAccessToken(res.data.accessToken)
-        setRefreshToken(res.data.refreshToken)
-        setExpiresIn(res.data.expiresIn)
+        localStorage.setItem("accessToken", res.data.accessToken)
+        localStorage.setItem("refreshToken", res.data.refreshToken)
+        localStorage.setItem("expiresIn", res.data.expiresIn)
+
+        refreshToken()
+        // window.history.pushState({}, null, "/")
       })
       .catch((err) => {
         console.log(err)
+        // window.location = "/"
       })
   }, [code])
 
-  useEffect(() => {
-    if (refreshToken || !expiresIn) return // If refreshToken hasn't been loaded into state yet
-    const interval = setInterval(() => {
-      console.log("MAKING REFRESH POST REQ")
-      axios
-        .post(server_url + "/refresh", { refreshToken })
-        .then((res) => {
-          setAccessToken(res.data.accessToken)
-          setExpiresIn(res.data.expiresIn)
-        })
-        .catch((err) => {
-          console.log("refresh ERROR")
-        })
-      // expiresIn is a set value -> we make the refresh call a minute before the token exires
-      // }, (expiresIn - 60) * 1000) // Count down til a minute before the token expires
-    }, (expiresIn - 60) * 1000)
+  // useEffect(() => {
+  //   if (refreshToken || !expiresIn) return // If refreshToken hasn't been loaded into state yet
+  //   const interval = setInterval(() => {
+  //     console.log("MAKING REFRESH POST REQ")
+  //     axios
+  //       .post(server_url + "/refresh", { refreshToken })
+  //       .then((res) => {
+  //         setAccessToken(res.data.accessToken)
+  //         setExpiresIn(res.data.expiresIn)
+  //       })
+  //       .catch((err) => {
+  //         console.log("refresh ERROR")
+  //         window.location = "/"
+  //       })
+  //     // expiresIn is a set value -> we make the refresh call a minute before the token exires
+  //     // }, (expiresIn - 60) * 1000) // Count down til a minute before the token expires
+  //   }, (expiresIn - 60) * 1000)
 
-    // If the refresh token accidentally resets, clear timeout
-    return () => clearInterval(interval)
-  }, [refreshToken, expiresIn])
-
-  return accessToken
+  //   // If the refresh token accidentally resets, clear timeout
+  //   return () => clearInterval(interval)
+  // }, [refreshToken, expiresIn])
 }
