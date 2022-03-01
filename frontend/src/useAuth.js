dbdcc4acffb73531baa2672c7ecad256adf8ecd4
seungguini <sl6369@nyu.edu>
2022-02-28 22:02:43 -0500

    Fix code parameter for useAuth

--- frontend/src/useAuth.js
+++ frontend/src/useAuth.js
@@ -2,7 +2,7 @@
  * Custom React Hook to handle Spotify authentication
  */
 
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 // Should EXCLUDE trailing slash ( / )
@@ -18,21 +18,41 @@ export default function useAuth(code) {
   // Update auth states whenever there is a new auth code from the client side (i.e. new update)
   useEffect(() => {
     console.log("useEffect in useAuth ran!")
-    console.log(server_url + "/login")
+    console.log(code)
     axios
       .post(server_url + "/login", { code })
       .then((res) => {
-        console.log("received response")
-        console.log(res.data)
+        console.log("setting values")
+        console.log(res)
         setAccessToken(res.data.accessToken)
         setRefreshToken(res.data.refreshToken)
         setExpiresIn(res.data.expiresIn)
-        console.log(res)
       })
       .catch((err) => {
         console.log(err)
       })
   }, [code])
+
+  useEffect(() => {
+    if (refreshToken || !expiresIn) return // If refreshToken hasn't been loaded into state yet
+    const interval = setInterval(() => {
+      console.log("MAKING REFRESH POST REQ")
+      axios
+        .post(server_url + "/refresh", { refreshToken })
+        .then((res) => {
+          setAccessToken(res.data.accessToken)
+          setExpiresIn(res.data.expiresIn)
+        })
+        .catch((err) => {
+          console.log("refresh ERROR")
+        })
+      // expiresIn is a set value -> we make the refresh call a minute before the token exires
+      // }, (expiresIn - 60) * 1000) // Count down til a minute before the token expires
+    }, (expiresIn - 60) * 1000)
+
+    // If the refresh token accidentally resets, clear timeout
+    return () => clearInterval(interval)
+  }, [refreshToken, expiresIn])
 
   return accessToken
 }
