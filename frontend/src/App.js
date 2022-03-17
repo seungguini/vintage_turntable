db5e6b1176632eaf66cd00e11dbc4ce086d60711
seungguini <sl6369@nyu.edu>
2022-03-17 17:18:56 -0400

    Initialize React-three-fiber

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import "./App.css"
+import ReactDOM from "react-dom"
+import React, { useRef, useState } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
 
 // Component Imports
 import Login from "./Login"
@@ -9,8 +12,42 @@ import "bootstrap/dist/css/bootstrap.min.css"
 // Grab Spotify Auth code from the URL
 const code = new URLSearchParams(window.location.search).get("code")
 
-function App() {
-  return code ? <Dashboard code={code} /> : <Login />
+const Box = (props) => {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+      {code ? <Dashboard code={code} /> : <Login />}
+    </div>
+  )
 }
 
 export default App
