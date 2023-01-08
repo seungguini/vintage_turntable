d0ea544f94f7d728f497ae2309f3d84ec4a6c37e
Seunggun <sl6369@nyu.edu>
2023-01-08 11:15:39 +0100

    update state to export custom hooks

--- frontend/src/states.js
+++ frontend/src/states.js
@@ -5,8 +5,19 @@ import create from "zustand";
 const usePlaybackStore = create((set) => ({
   isPlaying: false,
   volume: 100,
-  setPlaying: (input) => set((state) => ({ isPlaying: input })), // Typing would be very helpful
-  setVolume: (input) => set((state) => ({ volume: input })),
+  actions: {
+    // Separating actions from state : https://tkdodo.eu/blog/working-with-zustand#separate-actions-from-state
+    play: set({ isPlaying: true }),
+    pause: set({ isPlaying: false }),
+    mute: set({ volume: 0 }),
+    setVolume: (to) => set({ volume: to }),
+  },
 }));
 
-export { usePlaybackStore };
+// Custom hooks
+// helps prevent user from subscribing to the entire store
+// reference : https://tkdodo.eu/blog/working-with-zustand#only-export-custom-hooks
+export const useIsPlaying = () => usePlaybackStore((state) => state.isPlaying);
+export const useVolume = () => usePlaybackStore((state) => state.volume);
+export const usePlaybackActions = () =>
+  usePlaybackStore((state) => state.actions);
