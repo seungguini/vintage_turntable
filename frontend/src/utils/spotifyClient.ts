bf220c96a8f1fa8a83c8916b07c1f2efa25c0148
320834 <justinchen1000@gmail.com>
2023-10-01 16:44:30 -0400

    remove api call

--- frontend/src/utils/spotifyClient.ts
+++ frontend/src/utils/spotifyClient.ts
@@ -14,21 +14,3 @@ export const transferPlayback = (
     body: JSON.stringify({device_ids: [deviceId], play: shouldPlayOnTransfer})
   });
 };
-
-
-export const getAvailableDevices = (
-  token: string
-) : Promise<SpotifyApi.UserDevicesResponse | null> => {
-  return fetch(`${SPOTIFY_API_URL}/v1/me/player/devices`, {
-    method: "GET",
-    headers: {
-      "Authorization": `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    },
-  }).then((res : Response) => {
-    if(res.status === 200) {
-      return res.json()
-    }
-    return null;
-  });
-}
